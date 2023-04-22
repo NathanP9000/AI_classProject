c6cbce10556594ad1c4e8a265144451aafe25078
GrantBabin <grantbabin@gmail.com>
2023-04-21 23:03:53 -0500

    merging the conflicts

--- myTeam.py
+++ myTeam.py
@@ -114,6 +114,7 @@ class offenseAgent(CaptureAgent):
       if ( not gameState.getAgentState(self.index).isPacman and  closest < 4) or ( gameState.getAgentState(self.index).isPacman and  closest < 5): # If ghost is near u find shortest path back using bfs
         solution = [] #cannot hardcode this because it might be illegal. figure out better solution the problem is sometimes the stack empties out and nothing is returned
         stack = util.Queue()
+        print("Offense is running")
         counter = util.Counter()
         if self.isGoal(gameState):
           return Directions.STOP
@@ -139,33 +140,10 @@ class offenseAgent(CaptureAgent):
                 continue
               stack.push(child)
         if solution == []:
+          print("Random move")
           return  random.choice(actions)
-        print('eval time for agent %d: %.4f' % (self.index, time.time() - start))
+        #print('eval time for agent %d: %.4f' % (self.index, time.time() - start))
         return solution
-
-  def chooseAction(self, gameState):
-    """
-    Picks among the actions with the highest Q(s,a).
-    """
-    actions = gameState.getLegalActions(self.index) # south north ease est stop
-    # You can profile your evaluation time by uncommenting these lines
-    start = time.time()
-    
-    values = [self.evaluate(gameState, a) for a in actions] # Given legal actions. Evaluate each of them [10,30,-1300,1,3]
-    # print 'eval time for agent %d: %.4f' % (self.index, time.time() - start)
-    #print(values)
-
-    #Added code for detecting someone close to the pacman
-    myPos = gameState.getAgentState(self.index).getPosition()
-    enemies = [gameState.getAgentState(i) for i in self.getOpponents(gameState)]
-    defenders = [a for a in enemies if ((not a.isPacman) and (not a.scaredTimer > 5) and a.getPosition() != None)]
-    #print("self",self.index)
-    if len(defenders)> 0:
-      dists = [self.getMazeDistance(myPos, a.getPosition()) for a in defenders]
-      closest = min(dists)
-      if ( not gameState.getAgentState(self.index).isPacman and  closest < 4) or ( gameState.getAgentState(self.index).isPacman and  closest < 5): # If ghost is near u find shortest path back using bfs
-        print("Offense is running", closest)
-        return self.bfs(gameState,actions)
 
     maxValue = max(values)
     bestActions = [a for a, v in zip(actions, values) if v == maxValue] # Choose the best action
@@ -282,146 +260,6 @@ class offenseAgent(CaptureAgent):
 
     return entrances
 
-####################### 
-#Offensive Agent 2    #  
-#                     # 
-#######################
-class offenseAgent2(CaptureAgent):
-  """
-  A base class for reflex agents that chooses score-maximizing actions
-  """
- 
-  def registerInitialState(self, gameState):
-    self.start = gameState.getAgentPosition(self.index)
-    CaptureAgent.registerInitialState(self, gameState)
-    self.food = len(self.getFood(gameState).asList()) # holds number of food before crossing over
-
-  def isGoal(self,successor): #Gamestate
-    previousState = self.getPreviousObservation()
-    if ((not successor.getAgentState(self.index).isPacman)) : #Need to find specific agent id
-      return True
-    return False
-
-
-  def depthFirstSearch(self,previousState,gameState,depth):
-  
-    actions = gameState.getLegalActions(self.index)
-    if gameState.getAgentState(self.index).isPacman:
-      actions.remove('Stop')   
-  #  print(actions)
-    #enemies = [gameState.getAgentState(i) for i in self.getOpponents(gameState)]
-    #defenders = [a for a in enemies if ((not a.isPacman) and a.getPosition() != None)]
-    #dists = [self.getMazeDistance(myPos, a.getPosition()) for a in defenders]
-    #closest = min(dists)  
-    previousPosition = previousState.getAgentState(self.index).getPosition()
-    currentPosition = gameState.getAgentState(self.index).getPosition()
-    dist = self.getMazeDistance(previousPosition, currentPosition)
-    if dist > 1:
-      return -1000   
-    if(depth == 7):
-      if(self.index==0 or self.index==2):
-        blueFood = gameState.getBlueFood()
-        count=0
-        for r in blueFood:
-          for c in r:
-            if c is True:
-              count-=1
-        return count
-      else:
-        redFood = gameState.getRedFood()
-        count=0
-        for r in blueFood:
-          for c in r:
-            if c is True:
-              count-=1
-        return count
-    values = [self.depthFirstSearch(gameState,gameState.generateSuccessor(self.index,a),depth+1) for a in actions]
-    return max(values)
-  
-
-  ##########################################Choose ACtion
-  def chooseAction(self, gameState):
-    """
-    Picks among the actions with the highest Q(s,a).
-    """
-    actions = gameState.getLegalActions(self.index) # south north ease est stop
-    # You can profile your evaluation time by uncommenting these lines
-    start = time.time()
-    values = [self.evaluate(gameState,a) for a in actions] # Given legal actions. Evaluate each of them [10,30,-1300,1,3]
-
-
-    # IF SOMEONE IS CLOSE THAN DODGE THEM
-    myPos = gameState.getAgentState(self.index).getPosition()
-    enemies = [gameState.getAgentState(i) for i in self.getOpponents(gameState)]
-    defenders = [a for a in enemies if ((not a.isPacman) and (not a.scaredTimer > 10) and a.getPosition() != None)]
-    #print("self",self.index)
-    if len(defenders)> 0:
-      dists = [self.getMazeDistance(myPos, a.getPosition()) for a in defenders]
-      closest = min(dists)
-      if closest < 4: #if someone is close than dodge
-        values = [self.depthFirstSearch(gameState,gameState.generateSuccessor(self.index,a), 1) for a in actions]
-        print('eval time for agent %d: %.4f' % (self.index, time.time() - start))
-        print(values)
-    maxValue = max(values)
-    bestActions = [a for a, v in zip(actions, values) if v == maxValue] # Choose the action closest to food
-    foodLeft = len(self.getFood(gameState).asList())
-    if foodLeft <= 2:#Can food ever be less than 2?
-      bestDist = 9999
-      for action in actions:
-        successor = self.getSuccessor(gameState, action)
-        pos2 = successor.getAgentPosition(self.index)
-        dist = self.getMazeDistance(self.start,pos2)
-        if dist < bestDist:
-          bestAction = action
-          bestDist = dist
-      return bestAction
-
-    return random.choice(bestActions) #given 2 or more moves that have the same heuristic value choose one randomly
-
-
-
-  def getSuccessor(self, gameState, action):
-    """
-    Finds the next successor which is a grid position (location tuple).
-    """
-    successor = gameState.generateSuccessor(self.index, action)
-    pos = successor.getAgentState(self.index).getPosition()
-    if pos != nearestPoint(pos):
-      # Only half a grid position was covered
-      return successor.generateSuccessor(self.index, action)
-    else:
-      return successor
-
-  def evaluate(self, gameState, action):
-    """
-    Computes a linear combination of features and feature weights
-    """
-
-    features = self.getFeatures(gameState, action)
-    weights = self.getWeights(gameState, action)
-    #if self.index is 1:
-      #print(features)
-      #print(weights)
-      #print(features*weights) # the result is a single integer that is computed by multiplying out the common keys
-    return features * weights
-
-  def getFeatures(self, gameState, action):
-    features = util.Counter()
-    successor = self.getSuccessor(gameState, action)
-    foodList = self.getFood(successor).asList()    
-    features['successorScore'] = -len(foodList)#self.getScore(successor)
-    
-    # Compute distance to the nearest food
-    if len(foodList) > 0: # This should always be True,  but better safe than sorry
-      myPos = successor.getAgentState(self.index).getPosition()  
-      minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
-      features['distanceToFood'] = minDistance
-    #print(features)
-    return features
-
-  def getWeights(self, gameState, action):
-    return {'successorScore': 100,'distanceToFood': -1}
-
 
 class defenseAgent(CaptureAgent):
   """
@@ -474,6 +312,7 @@ class defenseAgent(CaptureAgent):
           stack.push(child)
     if solution == []:
       self.offense = False
+      print("Random move")
       return  random.choice(actions)
     #print('eval time for agent %d: %.4f' % (self.index, time.time() - start))
     self.offense = False
